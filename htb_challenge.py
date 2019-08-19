c31f69b686d1e64f869b9a45112db60e2b76303e
noodlemctwoodle <15075388+noodlemctwoodle@users.noreply.github.com>
2019-08-19 09:08:39 +0100

    init

---
+++ htb_challenge.py
@@ -0,0 +1,15 @@
+  
+import pickle
+
+# Used for opening the string
+x = open ('/root/Downloads/donotshare')
+# Load the file
+e = pickle.load(x)
+
+outstr = ''
+# Read every line
+for line in e: 
+    for char,n in line:
+        outstr += char*n
+    outstr =+ '\n'
+print outstr
